b12aa7f0eb7d4085facb01bfb3844dd50cf56fbe
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2021-05-28 13:26:03 +0200

    docs updated, tests optimized

--- inst/unitTests/test_simulateAMR.R
+++ inst/unitTests/test_simulateAMR.R
@@ -14,7 +14,7 @@ test_simulateAMR <- function () {
     length( simulateAMR(ramr.data, nsamples=1000, regions.per.sample=1000, samples.per.region=1000, merge.window=1000) ),
     143**2
   )
-  
+
   unique    <- simulateAMR(ramr.data, nsamples=29, regions.per.sample=2, samples.per.region=1)
   nonunique <- simulateAMR(ramr.data, nsamples=20, exclude.ranges=unique, merge.window=1000, regions.per.sample=5, samples.per.region=3)
   noise     <- simulateAMR(ramr.data, nsamples=100, exclude.ranges=c(unique,nonunique), merge.window=1, min.cpgs=1, max.cpgs=1, regions.per.sample=1000, samples.per.region=1)
@@ -23,9 +23,5 @@ test_simulateAMR <- function () {
   )
   RUnit::checkTrue(
     all(noise %outside% c(unique,nonunique))
-  )
-  RUnit::checkEquals(
-    sapply(list(unique, nonunique, noise), length),
-    c(58, 240, 1500)
   )
 }
