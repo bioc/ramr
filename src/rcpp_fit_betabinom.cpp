0305a7de407e9792b5b7f5acf3752a529ae7daef
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2025-01-22 20:29:47 +0100

    wbeta and betabinom stubs

---
+++ src/rcpp_fit_betabinom.cpp
@@ -0,0 +1,7 @@
+// Function estimates parameters of beta-binomial distribution (using count
+// data) and stores them in the vector of coefficients as
+// {[5] alpha (p), [6] beta (q), [7] log(std::beta)}
+
+// REFS:
+//  1) https://en.wikipedia.org/wiki/Beta-binomial_distribution#Method_of_moments
+
