2b84d034a6fc64450e5d040d2b972beed4864399
Oleksii.Nikolaienko <oleksii.nikolaienko@uib.no>
2025-02-12 12:10:03 +0100

    upd tests due to new API

--- inst/unitTests/test_getAMR.R
+++ inst/unitTests/test_getAMR.R
@@ -2,20 +2,20 @@ test_getAMR <- function () {
   data(ramr)
   
   RUnit::checkException(
-    getAMR(c())
+    getAMR(data.ranges=c())
   )
   RUnit::checkException(
-    getAMR(ramr.data, ramr.samples[1:2])
+    getAMR(data.ranges=ramr.data, data.samples=ramr.samples[1:2])
   )
   RUnit::checkException(
-    getAMR(ramr.data, c("a","b","c"))
+    getAMR(data.ranges=ramr.data, data.samples=c("a","b","c"))
   )
   RUnit::checkException(
-    getAMR(ramr.data, ramr.samples, ramr.method="zzz")
+    getAMR(data.ranges=ramr.data, data.samples=ramr.samples, compute="zzz")
   )
   
-  amr.iqr.1 <- getAMR(ramr.data, ramr.method="IQR", min.cpgs=5, merge.window=10000, iqr.cutoff=5, cores=1)
-  amr.iqr.2 <- getAMR(ramr.data, ramr.method="IQR", min.cpgs=5, merge.window=10000, iqr.cutoff=5, cores=2)
+  amr.iqr.1 <- getAMR(data.ranges=ramr.data, compute="IQR", combine.min.cpgs=5, combine.window=10000, combine.threshold=5) #, ncores=1)
+  amr.iqr.2 <- getAMR(data.ranges=ramr.data, compute="IQR", combine.min.cpgs=5, combine.window=10000, combine.threshold=5) #, ncores=2)
   RUnit::checkIdentical(
     amr.iqr.1,
     amr.iqr.2
@@ -25,27 +25,22 @@ test_getAMR <- function () {
     c(6, 45)
   )
   
-  amr.beta <- getAMR(ramr.data, ramr.samples, ramr.method="beta", min.cpgs=5, merge.window=10000, qval.cutoff=1e-3, cores=2)
+  amr.beta <- getAMR(data.ranges=ramr.data, data.samples=ramr.samples, compute="beta+binom", combine.min.cpgs=5, combine.window=10000, combine.threshold=1e-3)
   RUnit::checkEquals(
     c(sum(GenomicRanges::countOverlaps(amr.beta, ramr.tp.unique)), sum(GenomicRanges::countOverlaps(amr.beta, ramr.tp.nonunique))),
     c(6, 45)
   )
   
-  amr.wbeta <- getAMR(ramr.data, ramr.samples, ramr.method="wbeta", min.cpgs=5, merge.window=10000, qval.cutoff=1e-4, cores=2)
+  amr.wbeta <- getAMR(data.ranges=ramr.data, data.samples=ramr.samples, compute="beta+binom", compute.estimate="amle", compute.weights="sqrtInvDist",
+                      combine.min.cpgs=5, combine.window=10000, combine.threshold=1e-5)
   RUnit::checkEquals(
     c(sum(GenomicRanges::countOverlaps(amr.wbeta, ramr.tp.unique)), sum(GenomicRanges::countOverlaps(amr.wbeta, ramr.tp.nonunique))),
     c(6, 45)
   )
   
-  amr.iqr.3 <- getAMR(ramr.data, ramr.samples, ramr.method="IQR", min.cpgs=5, merge.window=10000, cores=2, exclude.range=c(0.1,0.9))
+  amr.iqr.3 <- getAMR(data.ranges=ramr.data, compute="IQR", combine.min.cpgs=5, combine.window=10000, combine.threshold=5, exclude.range=c(0.1,0.9))
   RUnit::checkEquals(
     c(sum(GenomicRanges::countOverlaps(amr.iqr.3, ramr.tp.unique)), sum(GenomicRanges::countOverlaps(amr.iqr.3, ramr.tp.nonunique))),
     c(2, 18)
-  )
-  
-  amr.beinf <- getAMR(ramr.data, ramr.samples, ramr.method="beinf", min.cpgs=5, merge.window=10000, qval.cutoff=1e-3, cores=2)
-  RUnit::checkEquals(
-    c(sum(GenomicRanges::countOverlaps(amr.beinf, ramr.tp.unique)), sum(GenomicRanges::countOverlaps(amr.beinf, ramr.tp.nonunique))),
-    c(6, 45)
   )
 }
--- inst/unitTests/test_plotAMR.R
+++ inst/unitTests/test_plotAMR.R
@@ -1,11 +1,11 @@
 test_plotAMR <- function () {
   data(ramr)
   RUnit::checkEquals(
-    length(plotAMR(ramr.data, ramr.samples, ramr.tp.nonunique)),
+    length(plotAMR(data.ranges=ramr.data, amr.ranges=ramr.tp.nonunique)),
     length(GenomicRanges::reduce(ramr.tp.nonunique))
   )
   RUnit::checkEquals(
-    length(plotAMR(ramr.data, NULL, ramr.tp.unique)),
+    length(plotAMR(data.ranges=ramr.data, amr.ranges=ramr.tp.unique)),
     length(GenomicRanges::reduce(ramr.tp.unique))
   )
 }
